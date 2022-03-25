52c56313307e809020b64e8d87438da226bf0898
ronnyzf <ronnyzarate@gmail.com>
2022-03-24 20:44:14 -0600

    update

--- flow.py
+++ flow.py
@@ -4,9 +4,16 @@ import re
 import subprocess, os
 import shutil
 import docker
+import statistics
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import EngFormatter
 
-area_array = []
-power_array = []
+area_array = [1,2]
+power_array = [1,2]
+NMED_array = [1,2]
+sum_array = []
 
 def replace(filename,oldText,newText,flags=0):
 	with open(filename,"r+") as file:
@@ -17,19 +24,50 @@ def replace(filename,oldText,newText,flags=0):
 		file.truncate()
 		file.write(fileContents)
 
-def prepare_chissel(apr_num,pri_type,sub_type):
+def prepare_chissel(apr_num,sum_type):
     print("Preparing Chissel \n \n")
     os.chdir('/home/prj/chisel-test/src/test/scala/RCA')
     subprocess.run("cp /home/prj/design-flow/sources/Launcher.scala Launcher.scala", shell=True)
     replace("Launcher.scala","APR_NUM_SUB",apr_num)
-    replace("Launcher.scala","PRI_TYPE_SUB",pri_type)
-    replace("Launcher.scala","SUB_TYPE_SUB",sub_type)
+    replace("Launcher.scala","TYPE_SUB",sum_type)
     
 
 def run_chissel():
     print("Run Chissel \n \n")
     os.chdir('/home/prj/chisel-test')
     subprocess.run("sbt 'test:runMain RCA.Launcher RCA_A'", shell=True)
+
+def extract_approx_error():
+    print(" \n \n Extract approximation error \n \n")
+    os.chdir('/home/prj/chisel-test/test_run_dir/RCA/RCA_A/')
+    #subprocess.run("pwd", shell=True)
+    pattern = re.compile("EXPECT AT")
+    found_lines = []
+    for i, line in enumerate(open('test.txt')):
+        for match in re.finditer(pattern, line):
+            found_lines.append(i)
+
+    extlines = []
+    file = open("test.txt", "rt")
+    for lines in file:
+        extlines.append(lines.rstrip('\n'))
+    file.close()
+    got_array = []
+    expected_array = []
+    for n in range(len(found_lines)):
+        parse=extlines[found_lines[n]].split(" ")
+        got_array.append(float(parse[9]))
+        expected_array.append(float(parse[11]))
+    
+    tmp_a = []
+    
+    for j in range(len(got_array)):
+        nmed_scalar = expected_array[j] - got_array[j]
+        tmp_a.append(nmed_scalar)
+    mean = statistics.mean(tmp_a)
+    NMED_array.append(mean)
+
+    #print(NMED_array)
 
 def clean_openlane_runs():
     print("Cleaning Openlane Runs \n \n")
@@ -116,24 +154,88 @@ def extract_power():
     parse=extlines[cnt+9].split(" ")
     power_array.append(float(parse[27]))
     print(power_array)
+
+def database_output(apr_num):
+    print(" \n \n Creating database \n \n")
+    os.chdir('/home/prj/design-flow/outputs/')
+    subprocess.run("pwd", shell=True)
+
+    with open ("database.csv", "w") as file:
+        writer = csv.writer(file, delimiter=',')
+        for n in range(len(area_array)):
+            writer.writerow([apr_num,sum_array[n],area_array[n],power_array[n],NMED_array[n]])
     
+def plot_data():
+    os.chdir('/home/prj/design-flow/')
+    l = len(area_array)
+    index = np.linspace(0, l, l)
+    print(index)
+
+    fig,ax=plt.subplots(1,1)
+    plt.subplots_adjust(bottom=0.2)
+    ax.plot(index, power_array,marker="v",lw=1)
+    plt.xlabel('Sum type')
+    plt.ylabel('Power (W)')
+    plt.title('Power consumed')
+    ax.grid(True)
+    #plt.tight_layout()
+    ax.set_xticks(index)
+    ax.set_xticklabels(sum_array, rotation='vertical', fontsize=6)
+    ax.legend(loc='upper right')
+    plt.savefig("outputs/power.png")
+    plt.close()
+
+    fig,ax=plt.subplots(1,1)
+    plt.subplots_adjust(bottom=0.2)
+    plt.plot(index, area_array,marker="v",lw=1)
+    plt.xlabel('Sum type')
+    plt.ylabel('Area (u^2)')
+    plt.title('Area')
+    ax.grid(True)
+    #plt.tight_layout()
+    ax.set_xticks(index)
+    ax.set_xticklabels(sum_array, rotation='vertical', fontsize=6)
+    ax.legend(loc='upper right')
+    plt.savefig("outputs/area.png")
+    plt.close()
     
+    fig,ax=plt.subplots(1,1)
+    plt.subplots_adjust(bottom=0.2)
+    ax.plot(index, power_array,marker="v",lw=1)
+    plt.plot(index, NMED_array,marker="v",lw=1)
+    plt.xlabel('Sum type')
+    plt.ylabel('NMED)')
+    plt.title('NMED')
+    ax.grid(True)
+    #plt.tight_layout()
+    ax.set_xticks(index)
+    ax.set_xticklabels(sum_array, rotation='vertical', fontsize=6)
+    ax.legend(loc='upper right')
+    plt.savefig("outputs/NMED.png")
+    plt.close()
 
 
 def main():
 
-    for apr_num in range(5):
-        for pri_type in range(3):
-            for sub_type in range(9):
-                clean_openlane_runs()
-                prepare_chissel(str(apr_num),str(pri_type),str(sub_type))
-                run_chissel()
-                prepare_openlane()
-                
-                run_openlane()
-                extract_area()
-                extract_power()
+    
+    sum_type_array = ["A0","A1","A2","A3","A4","A5","A6","A7","A8","A9","L0","T0","T1","T2","T3","T4","T5","T6"]
 
+    for a in range(len(sum_type_array)):
+        for b in range(len(sum_type_array)):
+            sum = sum_type_array[b]+","+sum_type_array[1]+",F0,F0,F0"
+            sum_array.append(sum)
+            clean_openlane_runs()
+            prepare_chissel(str(2),sum)
+            run_chissel()
+            extract_approx_error() # todas las distibuciones
+            prepare_openlane()
+            
+            run_openlane()
+            extract_area()
+            extract_power()
+    
+    database_output(2)
+    plot_data()
 
 if __name__ == "__main__":
     main()
--- sources/Launcher.scala
+++ sources/Launcher.scala
@@ -1,4 +1,4 @@
-// See LICENSE.txt for license details.
+ // See LICENSE.txt for license details.
 package RCA
 import chisel3._
 import chisel3.iotesters.{Driver, TesterOptionsManager}
@@ -6,8 +6,7 @@ import utils.TutorialRunner
 
 object Launcher {
   val apr_num = APR_NUM_SUB
-  val pri_type = PRI_TYPE_SUB
-  val sub_type = SUB_TYPE_SUB
+  val test = "TYPE_SUB"
   val tests = Map(
     "RCA" -> { (manager: TesterOptionsManager) =>
       Driver.execute(() => new RcaAdder(8), manager) {
@@ -15,14 +14,15 @@ object Launcher {
       }
     },
     "RCA_A" -> { (manager: TesterOptionsManager) =>
-      Driver.execute(() => new RcaAdder_A_1(8,apr_num,pri_type,sub_type), manager) {
+      Driver.execute(() => new RcaAdder_A_1(8,apr_num,test), manager) {
+        println("Actual Test",apr_num," ",test)
         (c) => new AdderTester_A_1(c,256)
       }
     }
   )
 
   def main(args: Array[String]): Unit = {
-    println (args)
     TutorialRunner("RCA", tests, args)
   }
 }
+
